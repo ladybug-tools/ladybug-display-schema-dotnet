0f6212a11c7b7f5883f65c2525bab6b7f22cb324
MingboPeng <mingo1214@gmail.com>
2022-08-17 19:20:28 -0400

    fix(interface): add IDisplay and IGeometry

---
+++ src/LadybugDisplaySchema/ManualAdded/Interface/IDisplay.cs
@@ -0,0 +1,8 @@
+﻿
+namespace LadybugDisplaySchema
+{
+    public interface IDisplay { }
+    public partial interface IDisplay2D : IDisplay { }
+    public partial interface IDisplay3D : IDisplay { }
+}
+
---
+++ src/LadybugDisplaySchema/ManualAdded/Interface/IGeometry.cs
@@ -0,0 +1,8 @@
+﻿
+namespace LadybugDisplaySchema
+{
+    public interface IGeometry { }
+    public partial interface IGeometry2D : IGeometry { }
+    public partial interface IGeometry3D : IGeometry { }
+}
+
