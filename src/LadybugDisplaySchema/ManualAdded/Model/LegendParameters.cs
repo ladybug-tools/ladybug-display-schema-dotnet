2da665ae3be8fd64e0cb90958e9290f7014eb726
MingboPeng <mingo1214@gmail.com>
2022-10-23 21:20:32 -0400

    fix(Legend): add more helpers to LegendParameters

--- src/LadybugDisplaySchema/ManualAdded/Model/LegendParameters.cs
+++ src/LadybugDisplaySchema/ManualAdded/Model/LegendParameters.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace LadybugDisplaySchema
 {
-    public partial class LegendParameters 
+    public partial class LegendParameters
     {
 
         protected LegendParameters()
@@ -12,19 +12,38 @@ namespace LadybugDisplaySchema
             this.Type = "LegendParameters";
         }
 
-        //public double X => (this.Properties2d..BasePlane?.O?.GetElementByIndex(0)).GetValueOrDefault();
+        //public double X => (this.Properties2d.BasePlane?.O?.GetElementByIndex(0)).GetValueOrDefault();
         //public double Y => (this.BasePlane?.O?.GetElementByIndex(1)).GetValueOrDefault();
         //public double Z => (this.BasePlane?.O?.GetElementByIndex(2)).GetValueOrDefault();
+        public double MinValue => GetValue(this.Min, 0);
+        public double MaxValue => GetValue(this.Max, 100);
 
-        public double MinValue => this.Min != null && this.Min.Obj is double d ? d : 0;
-        public double MaxValue => this.Max != null && this.Max.Obj is double d ? d : 100;
         //public double TextHeightValue => this.TextHeight != null && this.TextHeight.Obj is double d ? d : 12;
         public int SegmentCountValue => this.SegmentCount != null && this.SegmentCount.Obj is int d ? d : 11;
-        //public double SegmentWidthValue => this.SegmentWidth != null && this.SegmentWidth.Obj is double d ? d : 25;
-        //public double SegmentHeightValue => this.SegmentHeight != null && this.SegmentHeight.Obj is double d ? d : 36;
+        public double SegmentWidth2DValue {
+            get {
+                if (this.Properties2d == null)
+                    this.init2DDefault();
+                else
+                    this.Map2DLegendTo1080p();
+                return GetPxValue(this.Properties2d.SegmentWidth);
+            }
+        }
+        public double SegmentHeight2DValue
+        {
+            get
+            {
+                if (this.Properties2d == null)
+                    this.init2DDefault();
+                else
+                    this.Map2DLegendTo1080p();
+                return GetPxValue(this.Properties2d.SegmentHeight);
+            }
+        }
 
-        //public double Width => this.Vertical ? this.SegmentWidthValue : this.SegmentWidthValue * this.SegmentCountValue;
-        //public double Height => this.Vertical ? this.SegmentHeightValue * this.SegmentCountValue : this.SegmentHeightValue;
+        public double Width2D => this.Vertical ? this.SegmentWidth2DValue : this.SegmentWidth2DValue * this.SegmentCountValue;
+        public double Height2D => this.Vertical ? this.SegmentHeight2DValue * this.SegmentCountValue : this.SegmentHeight2DValue;
+
 
         public bool HasOrdinalDictionary => this.OrdinalDictionary != null && this.GetOrdinalDictionary().Count > 0;
 
@@ -48,19 +67,17 @@ namespace LadybugDisplaySchema
             Colors = numSegs > 1 ? c : new List<Color>() { c[0], c[0] };
         }
 
-        private void init2DDefault()
+        private Legend2DParameters init2DDefault()
         {
             this.Reset2DBaseLocation(10,100);
-            this.Properties2d.SegmentWidth = $"{25}px";
-            this.Properties2d.SegmentHeight = $"{36}px";
-
-            this.Properties2d.TextHeight = $"{12}px";
+            this.Map2DLegendTo1080p();
             Min = 0;
             Max = 100;
             SegmentCount = 11;
             DecimalCount = 2;
 
             Colors = _defaultColorSet.ToList();
+            return this.Properties2d;
         }
 
         private List<Color> _defaultColorSet = LegendColorSet.Presets.First().Value.ToList();
@@ -169,25 +186,92 @@ namespace LadybugDisplaySchema
 
         }
 
-        public void Reset2DBaseLocation(double X = default, double Y = default, double Z = default)
+        public void Reset2DBaseLocation(double X = default, double Y = default)
         {
             if (this.Properties2d == null) 
-                this.Properties2d = new Legend2DParameters(originX: $"{X}px", originY: $"{Y}px");
+                this.Properties2d = new Legend2DParameters(originX: $"{X}{_pxUnit}", originY: $"{Y}{_pxUnit}");
 
-            //if (this.Properties3d == null) 
-            //    this.Properties3d = new Legend3DParameters(new Plane(new Vector3D(0, 0, 1), new Point3D(X, Y, Z)));
-
-            //if (this.Properties3d.BasePlane == null)
-            //    this.Properties3d.BasePlane = new Plane(new Vector3D(0, 0, 1), new Point3D(X, Y, Z));
-
-            this.Properties2d.OriginX = $"{X}px";
-            this.Properties2d.OriginY = $"{Y}px";
+            this.Properties2d.OriginX = $"{X}{_pxUnit}";
+            this.Properties2d.OriginY = $"{Y}{_pxUnit}";
             
-            //var basePt = new Point3D(X, Y, Z);
-            //this.Properties3d.BasePlane.O = basePt.ToDecimalList();
         }
 
+        public void Reset3DBaseLocation(double X = default, double Y = default, double Z = default)
+        {
+            if (this.Properties3d == null)
+                this.Properties3d = new Legend3DParameters(new Plane(new Vector3D(0, 0, 1), new Point3D(X, Y, Z)));
 
+            if (this.Properties3d.BasePlane == null)
+                this.Properties3d.BasePlane = new Plane(new Vector3D(0, 0, 1), new Point3D(X, Y, Z));
+
+            var basePt = new Point3D(X, Y, Z);
+            this.Properties3d.BasePlane.O = basePt.ToDecimalList();
+        }
+
+        public Legend2DParameters Map2DLegendTo1080p() => Map2DLegendToPixel();
+        public Legend2DParameters Map2DLegendTo2160p() => Map2DLegendToPixel(2160, 3840);
+        public Legend2DParameters Map2DLegendToPixel(int viewWidth = 1080, int viewHeight = 1920)
+        {
+            var l = this.Properties2d ?? new Legend2DParameters();
+            l.TextHeight = ToPxValue(l.TextHeight, 12, viewHeight);
+            l.SegmentWidth = ToPxValue(l.SegmentWidth, 25, viewWidth);
+            l.SegmentHeight = ToPxValue(l.SegmentHeight, 36, viewHeight);
+            l.OriginX = ToPxValue(l.OriginX, 10, viewWidth);
+            l.OriginY = ToPxValue(l.OriginY, 100, viewHeight);
+            this.Properties2d = l;
+            return this.Properties2d;
+        }
+
+        private const string _pxUnit = "px";
+        private const string _percentUnit = "%";
+        private static string ToPxValue(AnyOf<Default, string> input, double defaultPxValue, int viewSize)
+        {
+            var value = defaultPxValue;
+            if (input == null || input.Obj is Default)
+                value = defaultPxValue;
+            else if (input.Obj is string ss)
+            {
+                ss = ss.ToLower().Trim();
+                if (ss.Contains(_pxUnit))
+                {
+                    ss = ss.Replace(_pxUnit, "");
+                    double.TryParse(ss, out value);
+                }
+                else if (ss.Contains(_percentUnit))
+                {
+                    ss = ss.Replace(_percentUnit, "");
+                    var done = double.TryParse(ss, out var percentValue);
+                    value = done? percentValue / 100 * viewSize : defaultPxValue;
+
+                }
+                else value = defaultPxValue;
+            }
+            else
+                value = defaultPxValue;
+
+            return $"{value}{_pxUnit}";
+        }
+        private static double GetPxValue(AnyOf<Default, string> input)
+        {
+            var value = 0.0;
+            if (input.Obj is string ss)
+            {
+                ss = ss.ToLower().Trim();
+                if (ss.Contains(_pxUnit))
+                {
+                    ss = ss.Replace(_pxUnit, "");
+                    double.TryParse(ss, out value);
+                }
+            }
+            else
+                value = 0.0;
+
+            return value;
+        }
+        private static double GetValue(AnyOf<Default, double> input, double defaultValue = default)
+        {
+            return input != null && input.Obj is double d ? d : defaultValue;
+        }
         //private double GetValue(AnyOf<Default, string> input, double defaultValue, out Unit unit)
         //{
 
